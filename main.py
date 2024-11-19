c3f1ebe606f5cfaba039a3ec97bf02c540c889ad
mandala siri <mandalasiri05@gmail.com>
2024-11-19 15:32:12 +0530

    hello

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Import Libraries and Load the Model
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import imdb
-from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import load_model
+from tensorflow.keras.datasets import imdb # type: ignore
+from tensorflow.keras.preprocessing import sequence # type: ignore
+from tensorflow.keras.models import load_model # type: ignore
 
 # Load the IMDB dataset word index
 word_index = imdb.get_word_index()
